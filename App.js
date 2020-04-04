2cb95085f03ce53314f92fefb22f95dc0f046677
ashminbhandari <abhandar@ramapo.edu>
2020-04-03 22:00:17 -0400

    Stack navigation setup

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
+//Basic react imports
 import React from 'react';
 import {View} from 'react-native';
-import { createStore, applyMiddleware, compose } from 'redux'; //For one stop store creation
+
+//Store, redux and thunk
+import {createStore, applyMiddleware, compose} from 'redux'; //For one stop store creation
 import reduxThunk from 'redux-thunk';
-import { Provider } from 'react-redux'; //Store provision for entire app
+import {Provider} from 'react-redux'; //Store provision for entire app
+
+//Navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from "@react-navigation/stack";
 
 //Custom components imports
 import LandingPage from './src/pages/LandingPage';
@@ -20,16 +27,20 @@ const devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_E
 const composedEnhancers = compose(thunk, devTool);
 
 //Create store
-const store =  createStore(
+const store = createStore(
     combinedReducer, composedEnhancers
 );
 
-export default function RotatingImageComponent() {
+const Stack = createStackNavigator();
+
+export default function App() {
     return (
         <Provider store={store}>
-            <View>
-                <LandingPage/>
-            </View>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Landing" component={LandingPage}/>
+                </Stack.Navigator>
+            </NavigationContainer>
         </Provider>
     );
 };
