486a9bc9b7b51d91eedd1dc5310ee3ec4b883baf
ashminbhandari <abhandar@ramapo.edu>
2020-04-05 22:03:14 -0400

    Authentication successfully configured using AppAuth

--- src/components/logical/AuthorizeButton.js
+++ src/components/logical/AuthorizeButton.js
@@ -50,9 +50,9 @@ const styles = StyleSheet.create(
 );
 
 let config = {
-    clientId: '<client_id>', // available on the app page
-    clientSecret: '<client_secret>', // click "show client secret" to see this
-    redirectUrl: 'com.myapp:/oauth', // the redirect you defined after creating the app
+    clientId: '08d55a470aef4302b9b3a82d93aa3f05', // available on the app page
+    clientSecret: 'c8dabbabf9fb4dfcb835dc3b8e8f26fb', // click "show client secret" to see this
+    redirectUrl: 'exp://127.0.0.1:19006', // the redirect you defined after creating the app
     scopes: ['user-read-email', 'playlist-modify-public', 'user-read-private'], // the scopes you need to access
     serviceConfiguration: {
         authorizationEndpoint: 'https://accounts.spotify.com/authorize',
@@ -97,3 +97,4 @@ async function refreshAuthAsync({ refreshToken }) {
     await cacheAuthAsync(authState);
     return authState;
 }
+
