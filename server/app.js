eb5963056f80ebe9b4fa2cc2cc9e52b9029d8737
ashminbhandari <abhandar@ramapo.edu>
2020-04-23 03:54:02 -0400

    server side spotify api setup, neater error handlings

--- server/app.js
+++ server/app.js
@@ -3,22 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+//Environment file
 require('dotenv').config();
 
-var authRouter = require('./routes/auth');
+//Routers
+var spotifyRouter = require('./routes/spotify');
 
+//Express app
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/auth', authRouter);
+app.use('/spotify', spotifyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/stores/authorizationStore.js
+++ src/stores/authorizationStore.js
@@ -1,13 +1,12 @@
-import { observable, action } from 'mobx';
+import {observable, action} from 'mobx';
 import axios from 'axios'; //For server calls (credentials)
 import * as AuthSession from "expo-auth-session";
-import {encode as btoa} from "base-64";
 
 //Get the user credentials from the server
 const getCredentials = async () => {
-    const res = await axios.get('http://192.168.1.138:3000/auth/spotifyCredentials');
-    const Credentials = res.data;
-    return Credentials;
+    const res = await axios.get('http://10.0.0.226:3000/spotify/credentials');
+    const credentials = res.data;
+    return credentials;
 };
 
 //The scope that we are going to be asking access for
@@ -16,12 +15,16 @@ const scopesArr = ['user-modify-playback-state', 'user-read-currently-playing', 
     'playlist-modify-private', 'user-read-recently-played', 'user-top-read']; //This is the scope of accessing that we will be asking for from the user
 const scopes = scopesArr.join(' ');
 
-//Gets the authorization code from Spotify (required for access tokens)
 const getAuthorizationCode = async () => {
     try {
+        //Spotify credentials from the server
         const credentials = await getCredentials();
+
+        //Session redirect URL
         const redirectUrl = AuthSession.getRedirectUrl();
-        const result = await AuthSession.startAsync({
+
+        //Auth session
+        const sessionResult = await AuthSession.startAsync({
             authUrl:
                 'https://accounts.spotify.com/authorize' +
                 '?response_type=code' +
@@ -30,53 +33,31 @@ const getAuthorizationCode = async () => {
                 (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
                 '&redirect_uri=' +
                 encodeURIComponent(redirectUrl),
-        })
-        return result.params.code;
-    } catch (err) {
-        console.error(err)
+        });
+
+        //Send back authorization code
+        return sessionResult.params.code;
+    } catch(err) {
+        console.log(err);
+        throw err;
     }
-};
+}
 
 //Auth store
 export class AuthorizationStore {
-    @observable isLogged = false;
     @observable accessToken = null;
-    @observable refreshToken = null;
-    @observable expirationTime = null;
 
     @action getAccessToken = async () => {
-        try {
-            const authorizationCode = await getAuthorizationCode();
-            const credentials = await getCredentials();
-            const credsB64 = btoa(`${credentials.clientId}:${credentials.clientSecret}`);
-            let requestBody = `grant_type=refresh_token&refresh_token=${refreshToken}`;
-            if (!this.expirationTime || new Date().getTime() > this.expirationTime) {
-                requestBody = `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${credentials.redirectUri}` //refresh if token expired
+            const authCode = await getAuthorizationCode();
+            try {
+                const response  = await axios.post('http://10.0.0.226:3000/spotify/token', {
+                    code: authCode
+                });
+                this.accessToken = response.data.token;
+                console.log("Access token received: " + this.accessToken);
+            } catch (err) {
+                console.log(err.response);
             }
-            const response = await fetch('https://accounts.spotify.com/api/token', {
-                method: 'POST',
-                headers: {
-                    Authorization: `Basic ${credsB64}`,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: requestBody,
-            });
-            const responseJson = await response.json();
-            const {
-                access_token: accessToken,
-                refresh_token: refreshToken,
-                expires_in: expiresIn,
-            } = responseJson;
-            const expirationTime = new Date().getTime() + expiresIn * 1000;
-            this.accessToken = accessToken;
-            this.refreshToken = refreshToken;
-            this.expirationTime = expirationTime;
-            console.log("The access token: ", accessToken);
-            console.log("The refresh token ", refreshToken);
-            console.log("The expiration time ", expirationTime);
-        } catch (err) {
-            console.error(err);
-        }
     }
 }
 
