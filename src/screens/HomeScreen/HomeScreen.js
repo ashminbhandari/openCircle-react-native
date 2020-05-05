b31956bd6235af7f203f769b95f482ecebbf2952
ashminbhandari <abhandar@ramapo.edu>
2020-05-04 21:01:54 -0400

    teeny update

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,48 +1,12 @@
 import React, {useState, useEffect} from "react";
 import {View, StyleSheet, Dimensions} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
 import mapStyle from './HomeStyle';
 import MapMarker from '../../components/UIElements/MapMarker';
-import randomGen from "../../utils/randomGen"; //To generate a random location
+import {useStores} from '../../hooks/useStores';
 
 export default function MapPage() {
-    const [userLocation, setUserLocation] = useState(null); //User's current location
-    const [locationPermission, setLocationPermission] = useState(false); //Doesn't have permission to check location at the start
-
-    useEffect(() => {
-        getLocationPermission();
-        getUserLocation();
-    }, [locationPermission]);
-
-    const getLocationPermission = async () => {
-        //No location permission? Get permission
-        if (!locationPermission) {
-            let {status} = await Permissions.askAsync(Permissions.LOCATION);
-            if (status === 'granted') {
-                setLocationPermission(true);
-            } else {
-                let longitude = randomGen(-180, 180, 3);
-                let latitude = randomGen(-90, 90, 3);
-                let location = {
-                    coords: {
-                        latitude: latitude,
-                        longitude: longitude
-                    }
-                };
-                setUserLocation(location);
-            }
-        }
-    };
-
-    const getUserLocation = async () => {
-        //If there is a permission then get the user location and set it in state
-        if (locationPermission) {
-            let location = await Location.getCurrentPositionAsync();
-            setUserLocation(location)
-        }
-    };
+    const {SpotifyStore} = useStores();
 
     return (
         <View style={styles.container}>
